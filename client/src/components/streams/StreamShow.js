944d62f06ad21fce2b8bb5e5c2fad2e2af8bcc5e
Maximilian Vorbrodt <maximilian.vorbrodt@hotmail.com>
2022-03-01 16:24:55 +0100

    finished stream application and hooked up to OBS

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,10 +1,44 @@
 import React from "react";
+import flv from "flv.js";
 import { connect } from "react-redux";
 import { fetchStream } from "../../actions";
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    if (this.flvPlayer) {
+      this.flvPlayer.destroy();
+    }
+  }
+
+  buildPlayer() {
+    if (this.flvPlayer || !this.props.stream) {
+      return;
+    }
+    const { id } = this.props.match.params;
     this.props.fetchStream(this.props.match.params.id);
+    this.flvPlayer = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.flvPlayer.attachMediaElement(this.videoRef.current);
+    this.flvPlayer.load();
   }
 
   render() {
@@ -14,7 +48,8 @@ class StreamShow extends React.Component {
     const { title, description } = this.props.stream;
 
     return (
-      <div className="">
+      <div>
+        <video ref={this.videoRef} style={{ width: "100%" }} controls={true} />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
